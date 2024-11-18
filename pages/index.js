6107a2c45cc620c06b6d5294a9e0a369e2aef89d
pkmirandaa <patrickbatista0512@icloud.com>
2024-11-18 19:18:43 -0300

    teste de Link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link>
+        <a href="/">Acessar a página Sobre</a>
+      </Link>
+    </div>
+  );
 }
 
 export default Home;
---
+++ pages/sobre.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+function Sobre() {
+  return (
+    <div>
+      <h1>Sobre</h1>
+      <Link>
+        <a href="/">Acessar a página Home</a>
+      </Link>
+    </div>
+  );
+}
+
+export default Sobre;
